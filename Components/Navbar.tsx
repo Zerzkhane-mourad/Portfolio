c9cd8f9605169c1ad9bcc06c11fc718787b15012
zerzkhane <zerzkhanemourad@gmail.com>
2023-12-29 21:53:58 +0100

    add Navbar

---
+++ Components/Navbar.tsx
@@ -0,0 +1,25 @@
+import { Bars3Icon } from "@heroicons/react/16/solid"
+
+const Navbar = () => {
+  return (
+    <div className="w-[100%] top-0 h-[12vh] bg-[#141c27] shadow-md">
+        <div className="flex items-center justify-between w-[80%] mx-auto h-[100%]">
+            <h1 className="flex-[0.6] cursor-pointer text-[25px] text-white font-bold">
+                WEB
+                <span className="text-yellow-500" >DEVELOPER</span>
+            </h1>
+            <p className="nav-link">Home</p>
+            <p className="nav-link">Services</p>
+            <p className="nav-link">About</p>
+            <p className="nav-link">Projects</p>
+            <p className="nav-link">Blog</p>
+            <p className="nav-link">contact</p>
+            <div>
+                <Bars3Icon className="w-[2rem] md:hidden h-[2rem] cursor-pointer text-yellow-300"/>
+            </div>
+        </div>
+    </div> 
+  )
+}
+
+export default Navbar
---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import Navbar from '@/Components/Navbar'
+
+const HomePage = () => {
+  return (
+    <div className='overflow-x-hidden'>
+      <Navbar/>
+
+    </div>
+  )
+}
+
+export default HomePage
