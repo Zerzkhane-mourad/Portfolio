2e9261d3cd6da31eee5ff7f694ff6366c75a0ff0
zerzkhane <zerzkhanemourad@gmail.com>
2023-12-30 21:59:14 +0100

    add hero section

--- Components/Hero.tsx
+++ Components/Hero.tsx
@@ -1,9 +1,22 @@
+import Image from "next/image"
 import Particle from "./Particle"
+import TextAnimation from "./TextAnimation"
 
 const Hero = () => {
     return (
         <div className="h-[88vh] bg-[url('/images/banner.jpg')] bg-cover bg-center">
             <Particle />
+            <div className="w-[80%] grid-cols-1 mx-auto grid lg:grid-cols-2 gap-[3rem] h-[100%] items-center">
+                <div>
+                    <h1 className="text-[35px] md:text-[50px] text-white font-bold">
+                        HI, I&apos;M <span className="text-yellow-500">MOURAD</span>
+                    </h1>
+                    <TextAnimation />
+                </div>
+                <div className="w-[500px] h-[500px] hidden relative lg:flex items-center rounded-full ">
+                    <Image src="/images/7O4.jpg" alt="user" layout="fill" className="object-cover rounded-full" />
+                </div>
+            </div>
         </div>
     )
 }
---
+++ Components/TextAnimation.tsx
@@ -0,0 +1,23 @@
+import { TypeAnimation } from 'react-type-animation';
+
+const TextAnimation = () => {
+  return (
+    <TypeAnimation
+      sequence={[
+        'Coder',
+        1500, 
+        'Web Developer',
+        1500,
+        'designer',
+        1500,
+        'Progrmmer',
+        1500
+      ]}
+      speed={50}
+      className='text-[2rem] md:text-[3rem] text-white font-bold uppercase'
+      repeat={Infinity}
+    />
+  );
+};
+
+export default TextAnimation
