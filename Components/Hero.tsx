0a50afae5602534799435380d4512ebf16b1e998
zerzkhane <zerzkhanemourad@gmail.com>
2024-01-02 02:34:40 +0100

    add button dowlaod resume

--- Components/Hero.tsx
+++ Components/Hero.tsx
@@ -1,8 +1,21 @@
 import Image from "next/image"
 import Particle from "./Particle"
 import TextAnimation from "./TextAnimation"
+import { ArrowDownTrayIcon } from "@heroicons/react/16/solid"
 
 const Hero = () => {
+
+
+    const onButtonClick = () => {
+        const pdfUrl = "CvMouradZerzkhane.pdf";
+        const link = document.createElement("a");
+        link.href = pdfUrl;
+        link.download = "resume.pdf";
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    };
+
     return (
         <div className="h-[88vh] bg-[url('/images/banner.jpg')] bg-cover bg-center">
             <Particle />
@@ -13,11 +26,17 @@ const Hero = () => {
                     </h1>
                     <TextAnimation />
                     <p className="mt-[2rem] text-[20px] text-[#ffffff92] ">
-                    I proficiently handle both frontend and backend technologies, crafting dynamic and responsive web applications. Proficient in using frameworks like React, Angular, or Vue for frontend and Node.js or Express.js for backend development. Skilled in database management using MongoDB, SQL, or other modern databases. Experienced in creating RESTful APIs, ensuring smooth communication between the server and client. Committed to delivering high-quality, scalable solutions while staying updated with the latest industry trends and best practices.
+                        Passionate Fullstack JavaScript Developer proficient in modern frameworks and libraries, crafting innovative solutions for seamless web development. Check out my portfolio showcasing dynamic front-end designs and robust back-end systems.
                     </p>
+                    <div className="mt-[2rem] flex-col space-y-6 sm:space-y-0 sm:flex sm:flex-row items-center sm:space-x-6">
+                        <button onClick={onButtonClick} className="flex items-center space-x-2  px-[2rem] hover:bg-yellow-500 transition-all  duration-200 py-[1rem] text-[18px] text-black font-bold bg-slate-600 ">
+                            <b>Dowload Cv</b>
+                            <ArrowDownTrayIcon className="w-[1.6rem] h-[1.7rem] text-black" />
+                        </button>
+                    </div>
                 </div>
                 <div className="w-[500px] h-[500px] hidden relative lg:flex items-center rounded-full ">
-                    <Image src="/images/7O4.jpg" alt="user" layout="fill" className="object-cover rounded-full" />
+                    <Image src="/images/7O5.jpg" alt="user" layout="fill" className="object-cover rounded-full" />
                 </div>
             </div>
         </div>
