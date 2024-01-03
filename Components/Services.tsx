843117749c442d8bb08310c58885dd37db7eb4d4
zerzkhane <zerzkhanemourad@gmail.com>
2024-01-03 19:46:57 +0100

    add services fullStackand backend

--- Components/Services.tsx
+++ Components/Services.tsx
@@ -1,4 +1,4 @@
-import { CodeBracketSquareIcon } from "@heroicons/react/16/solid"
+import { CodeBracketSquareIcon, CommandLineIcon, RocketLaunchIcon } from "@heroicons/react/16/solid"
 
 const Services = () => {
     return (
@@ -7,7 +7,7 @@ const Services = () => {
             <p className="heading">
                 My <span className="text-yellow-500">Services</span>
             </p>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 w-[80%] mx-auto items-center gap-3 mt-[4rem] text-white">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 w-[80%] mx-auto items-center gap-[2.5rem] mt-[4rem] text-white">
                 <div>
                     <div className="bg-red-700 hover:scale-110 transform transition-all duration-300 hover:-rotate-6 font-semibold text-center p-[2rem]">
                         <CodeBracketSquareIcon className="w-[6rem] h-[6rem] mx-auto text-[#d3fae8]" />
@@ -19,8 +19,29 @@ const Services = () => {
                         </p>
                     </div>
                 </div>
-                <div>FullStak</div>
-                <div>Backend</div>
+                <div>
+                    <div className="bg-blue-400 hover:scale-110 transform transition-all duration-300 font-semibold text-center p-[2rem]">
+                        <CommandLineIcon className="w-[6rem] h-[6rem] mx-auto text-[#d3fae8]" />
+                        <h1 className="text-[20px] md:text-[30px] my-[1.5rem]">FullStak</h1>
+                        <p className="text-[15px] text-[#d3d2d2] font-normal">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            Voluptatem eos quasi nesciunt animi possimus la
+                            udantium atque veritatis facere recusandae eaque?
+                        </p>
+                    </div>
+                </div>
+                <div>
+                    <div className="bg-orange-500 hover:scale-110 transform transition-all duration-300 hover:-rotate-6 font-semibold text-center p-[2rem]">
+                        <RocketLaunchIcon className="w-[6rem] h-[6rem] mx-auto text-[#d3fae8]" />
+                        <h1 className="text-[20px] md:text-[30px] my-[1.5rem]">Backend</h1>
+                        <p className="text-[15px] text-[#d3d2d2] font-normal">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            Voluptatem eos quasi nesciunt animi possimus la
+                            udantium atque veritatis facere recusandae eaque?
+                        </p>
+                    </div>
+                </div>
+
             </div>
         </div>
     )
